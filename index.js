ef71c0bd3c685f271c70ee99513d62eefbd3f55f
Navneet <navneetburman900@gmail.com>
2023-03-27 16:28:31 +0530

    changes made

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get("/",(req,res)=>{
-    res.status(200).send("Welecome to Ravi Sharma TODO api")
+    res.status(200).send("Welecome to Navnet api")
 })
 
 app.use("/users",UserRouter)
